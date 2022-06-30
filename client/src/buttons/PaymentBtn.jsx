cf2716baf7c866b2daddd2e3b6ac12b922788bd4
yivanov <apereliez1@gmail.com>
2022-06-30 17:29:57 +0300

    set new tab

--- client/src/buttons/PaymentBtn.jsx
+++ client/src/buttons/PaymentBtn.jsx
@@ -1,12 +1,12 @@
 
 const PaymentButton = ({ value1, value2 }) => {
     return (
-        <form method="POST" action="https://www.liqpay.ua/api/3/checkout" acceptCharset="utf-8">
+        <form target="_blank" method="POST" action="https://www.liqpay.ua/api/3/checkout" acceptCharset="utf-8">
             <input type="hidden" name="data" 
             value={value1} />
             <input type="hidden" name="signature" value={value2} />
             <div className='input-container'>
-                <button type='submit' className='brown checkout-btn'>Оплатити</button>
+                <button type='submit' target="_blank" className='brown checkout-btn'>Оплатити</button>
             </div>
         </form>
     )
