d8b7fbd77977554e7278e17b67f247aa5b4c9157
yivanov <apereliez1@gmail.com>
2022-06-30 15:37:51 +0300

    update parse html

--- server/app/liqpay/pay.js
+++ server/app/liqpay/pay.js
@@ -19,8 +19,13 @@ const createOrderBTN = (amount, description, order_id, action = "pay", currency 
 }
 
 function ParseHTML(html) {
-    let str = html;
-    str = str.split(`<form method="POST" action="https://www.liqpay.ua/api/3/checkout" accept-charset="utf-8"><input type="hidden" name="data" value="`)[1];
-    str = str.split(`" /><input type="hidden"`)[0];
-    return str;
+    let str1 = html;
+    str1 = str1.split(`<form method="POST" action="https://www.liqpay.ua/api/3/checkout" accept-charset="utf-8"><input type="hidden" name="data" value="`)[1];
+    let str2 = str1;
+    str1 = str1.split(`" /><input type="hidden"`)[0];
+
+    
+    str2 = str2.split(`<input type="hidden" name="signature" value="`)[1];
+    str2 = str2.split(`" /><input type="image"`)[0];
+    return [str1, str2];
 }
